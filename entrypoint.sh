b8610699d50423eaee435deddb76b891b2b86b11
xiataosky <xiataosky@163.com>
2018-10-29 03:46:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,22 @@ if [ "$VER" = "latest" ]; then
 else
   wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
   unzip v2ray.zip 
-  cd /v2raybin/v2ray-v$VER-linux-64
+  mv /v2raybin/v2ray-v$VER-linux-64/v2ray .
+  mv /v2raybin/v2ray-v$VER-linux-64/v2ctl .
+  mv /v2raybin/v2ray-v$VER-linux-64/geoip.dat .
+  mv /v2raybin/v2ray-v$VER-linux-64/geosite.dat .
 fi
 
 chmod +x v2ray
 chmod +x v2ctl
 
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
+#echo -e -n "$CONFIG_JSON1" > config.json
+#echo -e -n "$PORT" >> config.json
+#echo -e -n "$CONFIG_JSON2" >> config.json
+#echo -e -n "$UUID" >> config.json
+#echo -e -n "$CONFIG_JSON3" >> config.json
+
+sed -i "s/66666/$PORT/g" config.json
+sed -i "s/your_uuid/$UUID/g" config.json
 
 ./v2ray
